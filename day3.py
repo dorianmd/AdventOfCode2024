2b9e1bafe2c47ffb402426b290fb9e06c44bbe50
dorianmd <dorian.madrzycki@gmail.com>
2025-01-21 19:37:52 +0100

    Advent of Code Day 4

--- day3.py
+++ day3.py
@@ -1,4 +1,50 @@
 # Advent of Code 2024
 # Day 3: https://adventofcode.com/2024/day/1
 # Description:
+import re
 
+
+class AdventDay3:
+    def __init__(self):
+        self.mults1 = []
+        with open('inputs\day3.txt') as f:
+            self.data = f.read()
+        self.mults1 = self.find_mults(self.data, self.mults1)
+        self.valid_instructions_finder()
+        self.res1 = 0
+        self.res2 = 0
+
+    def get_mult_score(self):
+        for pair in self.mults1:
+            self.res1 += pair[0] * pair[1]
+
+    def get_mult_w_instructions(self):
+        self.find_mults_w_instructions()
+
+    def find_mults(self, string, arr):
+        if string:
+            for i in re.findall(r'mul\(\d+,\d+\)', string):
+                numbers = (re.findall(r'\d+', i))
+                arr.append([int(num) for num in numbers])
+            return arr
+
+    def valid_instructions_finder(self):
+        self.valid_instructions = re.findall(r"mul\(\d+,\d+\)|do\(\)|don't\(\)", self.data)
+
+    def find_mults_w_instructions(self):
+        mult = True
+        for i in self.valid_instructions:
+            if i == 'do()':
+                mult = True
+            elif i == "don't()":
+                mult = False
+            elif mult:
+                x = re.findall(r'\d+', i)
+                self.res2 += int(x[0]) * int(x[1])
+
+
+
+a = AdventDay3()
+a.get_mult_score()
+a.get_mult_w_instructions()
+print(a.res1, a.res2)
---
+++ day4.py
@@ -0,0 +1,63 @@
+class AdventDay4:
+    def __init__(self):
+        self.lines = []
+        with open('inputs/day4.txt') as f:
+            for line in f.readlines():
+                self.lines.append([x for x in line if x != '\n'])
+        for line in self.lines:
+            print(line)
+        self.word = ['XMAS', 'SAMX']
+        self.word2 = ['MAS', 'SAM']
+        self.n =  len(self.lines)
+        self.m = len(self.lines[0])
+        self.res1 = 0
+        self.res2 = 0
+
+    def find_word(self):
+        self._find_row()
+        self._find_col()
+        self._find_diag()
+
+    def _find_row(self):
+        for row in self.lines:
+            for i in range(self.m-  3):
+                self.res1 += ''.join(row[i:i+4]) in self.word
+
+    def _find_col(self):
+        for col in zip(*self.lines):
+            for i in range(self.m - 3):
+                self.res1 += ''.join(col[i:i+4]) in self.word
+
+
+    def _find_diag(self):
+        for row in range(self.n-3):
+            for col in range(self.m-3):
+                diag1 = ''.join([self.lines[row][col],
+                                 self.lines[row+1][col+1],
+                                 self.lines[row+2][col+2],
+                                 self.lines[row+3][col+3]])
+                diag2 = ''.join([self.lines[row][col+3],
+                                 self.lines[row+1][col+2],
+                                 self.lines[row+2][col+1],
+                                 self.lines[row+3][col]])
+                self.res1 += diag1 in self.word
+                self.res1 += diag2 in self.word
+
+    def find_mas(self):
+        for row in range(self.n-2):
+            for col in range(self.m-2):
+                diag1 = ''.join([self.lines[row][col],
+                                 self.lines[row + 1][col + 1],
+                                 self.lines[row + 2][col + 2]])
+                diag2 = ''.join([self.lines[row][col + 2],
+                                 self.lines[row + 1][col + 1],
+                                 self.lines[row + 2][col]])
+                self.res2 +=1 if diag1 in self.word2 and diag2 in self.word2 else 0
+
+
+
+
+a = AdventDay4()
+a.find_word()
+a.find_mas()
+print(a.res1, a.res2)
